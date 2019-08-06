05b0a682d4f18a4a463decaf10467750836670c0
yuanjin <yuanjin@jd.com>
2019-08-06 19:59:15 +0800

    assignment04

--- assignment_04/wordToVec.py
+++ assignment_04/wordToVec.py
@@ -1,140 +1,111 @@
 import jieba
-import gensim
+from gensim.models import Word2Vec
+from gensim.models.word2vec import LineSentence
 import logging
 import time
 from functools import wraps
 import os
 import re
-import pickle
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
-import matplotlib
 from opencc import OpenCC
 
-# 第一步，创建一个logger
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)  # Log等级总开关
-# 第二步，创建一个handler，用于写入日志文件
-# rq = time.strftime('%Y%m%d%H%M', time.localtime(time.time()))
-# log_path = os.path.dirname(os.getcwd()) + '/Logs/'
-# log_name = log_path + rq + '.log'
-# logfile = log_name
-# fh = logging.FileHandler(logfile, mode='w')
-fh = logging.FileHandler('word2vec.log', mode='w')
-fh.setLevel(logging.INFO)  # 输出到file的log等级的开关
-# 第三步，定义handler的输出格式
-formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
-fh.setFormatter(formatter)
-# 第四步，将logger添加到handler里面
-logger.addHandler(fh)
+
+def save_log():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    handler = logging.FileHandler('word2vec.log', mode='w')
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+    return logger
 
 
-# 日志
-# logger.debug('this is a logger debug message')
-# logger.info('this is a logger info message')
-# logger.warning('this is a logger warning message')
-# logger.error('this is a logger error message')
-# logger.critical('this is a logger critical message')
-
-
-def stopwords(line):
-    # 处理停止词
-    with open(STOP_WORD, 'r') as fr:
-        words = fr.read()
-    res = [x for x in line if x not in words]
-    return res
+def is_chinese(word):
+    for ch in list(word):
+        if ch < u'\u4e00' or ch > u'\u9fff':
+            return False
+    return True
 
 
 def decorator(func):
     @wraps(func)
     def wrap():
         start_time = time.time()
-        logger.info('进入{}函数，开始时间：{}'.format(func.__name__, start_time))
+        logger.info('input {} function，start time：{}'.format(func.__name__, start_time))
         func()
-        logger.info('{}函数执行结束，总用时时间：{}'.format(func.__name__, str(time.time() - start_time)))
-        logger.info('*' * 50 + '分割线' + '*' * 50)
+        logger.info('{} function run end，running spends：{} second'.format(func.__name__, str(time.time() - start_time)))
+        logger.info('*' * 50 + ' spilt line ' + '*' * 50)
 
     return wrap
 
 
 @decorator
 def cut_word():
-    cc = OpenCC('hk2s')
-    for file_name in os.listdir(CORPUS):
-        res = re.search('^\.', file_name)
-        if file_name == 'cut_result':
-            break
-        if file_name not in ['.DS_Store', 'cut_result', 'cut_result_bak'] and not res:
-            with open(CORPUS + file_name, 'r') as fr:
-                rds = fr.readlines()
-                logger.info('当前文件行数: {}'.format(len(rds)))
-                with open(CUT_RESULT, 'a') as fw:
-                    for r in rds:
-                        re_filter = re.findall('<doc id=.*>|</doc>', r)
-                        if not re_filter:
-                            clean_word = ' '.join(stopwords(jieba.lcut(cc.convert(r))))
-                            if clean_word:
-                                fw.write(clean_word)
-                        else:
-                            logger.info('过滤字符: {}'.format(re_filter[0]))
+    cc = OpenCC('t2s')
+    for parent, dir_names, file_names in os.walk(DATA_PATH, followlinks=True):
+        for filename in file_names:
+            if re.match(r'^w', filename):
+                file_path = os.path.join(parent, filename)
+                output = open(CUT_WORDS_RESULT, 'w', encoding='utf-8')
+                with open(file_path, 'r', encoding='utf-8') as content:
+                    for text_num, line in enumerate(content):
+                        line = line.strip('\n')
+                        if not line.strip(): continue
+                        if re.findall('<.*>', line): continue
+                        words = jieba.cut(cc.convert(line), cut_all=False)
+                        for word in words:
+                            if is_chinese(word):
+                                output.write(word + ' ')
+                        output.write('\n')
+                output.close()
+                logger.info('file name：{} finished!'.format(filename))
+                print('file name：{} finished!'.format(filename))
 
 
 @decorator
 def train_model():
-    sentences = gensim.models.word2vec.LineSentence(CUT_RESULT)
-    model = gensim.models.word2vec.Word2Vec(sentences, size=100, hs=1, min_count=200, window=3)
-    model.save(MODEL_OBJECT)
-    try:
-        sim1 = model.similarity('初等数学', '代数')
-    except KeyError:
-        sim1 = 0
-    print(sim1)
+    sentences = LineSentence(CUT_WORDS_RESULT)
+    model = Word2Vec(sentences, size=300, min_count=100, window=7)
+    model.save(MODEL_PATH)
 
 
 @decorator
 def predict_model():
-    model = gensim.models.Word2Vec.load(MODEL_OBJECT)
+    model = Word2Vec.load(MODEL_PATH)
+    s_1, s_2 = '', ''
     try:
-        sim1 = model.similarity('爱情', '友情')
-        sim2 = model.most_similar(['世界杯'])
-
+        # 查看两个词相似度
+        s_1 = model.similarity('爱情', '友情')
+        # 查看和 这个词最相关的几个词
+        s_2 = model.most_similar('数学')
     except KeyError:
-        sim1 = 0
-    print(sim1)
-    print(sim2)
-    print(model['世界杯'])
-
-
-def main(flag):
-    cut_word()
-    if flag == '1':
-        if 'model_object' not in os.listdir('../static/'):
-            train_model()
-    else:
-        if 'cut_result_bak' not in os.listdir('../../wikiex/'):
-            train_model()
-    # predict_model()
+        pass
+    print(s_1)
+    print(s_2)
+    # 查看这个词的 词向量
+    print(model.wv['数学'])
 
 
 def tsne_plot(model):
+    "Creates and TSNE model and plots it"
     labels = []
     tokens = []
-    print(model['民族'])
     for word in model.wv.vocab:
         tokens.append(model[word])
         labels.append(word)
 
     tsne_model = TSNE(perplexity=40, n_components=2, init='pca', n_iter=2500, random_state=23)
     new_values = tsne_model.fit_transform(tokens)
-
     x = []
     y = []
-
     for value in new_values:
         x.append(value[0])
         y.append(value[1])
-
-    plt.figure(figsize=(16, 16))
+    plt.figure(figsize=(8, 8))
     for i in range(len(x)):
         plt.scatter(x[i], y[i])
         plt.annotate(labels[i],
@@ -147,25 +118,12 @@ def tsne_plot(model):
 
 
 if __name__ == '__main__':
-    flag = input('本地输入0，服务器输入1, 其他值仅仅获取训练结果做图形可视化---> ：')
-    if flag == '1':
-        STOP_WORD = '../static/stopwords'
-        CORPUS = '../corpus/'
-        CUT_RESULT = '../static/cut_result'
-        MODEL_OBJECT = '../static/model_object'
-        main(flag)
-    elif flag == '0':
-        STOP_WORD = 'stopwords'
-        CORPUS = '../../wikiex/'
-        CUT_RESULT = '../../wikiex/cut_result'
-        MODEL_OBJECT = '../../wikiex/cut_result_bak'
-        main(flag)
-        model = gensim.models.Word2Vec.load(MODEL_OBJECT)
-        tsne_plot(model)
-    elif flag == '2':
-        MODEL_OBJECT = '../../wikiex/cut_result_bak'
-        predict_model()
-    else:
-        MODEL_OBJECT = '../../wikiex/cut_result_bak'
-        model = gensim.models.Word2Vec.load(MODEL_OBJECT)
-        tsne_plot(model)
+    logger = save_log()
+    DATA_PATH = '../../zhwiki/'
+    CUT_WORDS_RESULT = 'cut_words_result'
+    MODEL_PATH = 'model.Word2Vec'
+    cut_word()
+    train_model()
+    model = Word2Vec.load(MODEL_PATH)
+    predict_model()
+    tsne_plot(model)
