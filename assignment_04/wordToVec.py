97bbd279cc1e23d83282da84da2c1c0de1ecd1ad
yuanjin <yuanjin@jd.com>
2019-08-07 16:20:10 +0800

    assignment04-completed

--- assignment_04/wordToVec.py
+++ assignment_04/wordToVec.py
@@ -11,19 +11,6 @@ import matplotlib.pyplot as plt
 from opencc import OpenCC
 
 
-def save_log():
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-
-    handler = logging.FileHandler('word2vec.log', mode='w')
-    handler.setLevel(logging.INFO)
-    formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
-    handler.setFormatter(formatter)
-
-    logger.addHandler(handler)
-    return logger
-
-
 def is_chinese(word):
     for ch in list(word):
         if ch < u'\u4e00' or ch > u'\u9fff':
@@ -37,7 +24,8 @@ def decorator(func):
         start_time = time.time()
         logger.info('input {} function，start time：{}'.format(func.__name__, start_time))
         func()
-        logger.info('{} function run end，running spends：{} second'.format(func.__name__, str(time.time() - start_time)))
+        logger.info(
+            '{} function run end，this func spends：{} second'.format(func.__name__, str(time.time() - start_time)))
         logger.info('*' * 50 + ' spilt line ' + '*' * 50)
 
     return wrap
@@ -46,11 +34,12 @@ def decorator(func):
 @decorator
 def cut_word():
     cc = OpenCC('t2s')
+    output = open(CUT_WORDS_RESULT, 'w', encoding='utf-8')
     for parent, dir_names, file_names in os.walk(DATA_PATH, followlinks=True):
+        logger.info('dir name：{} start'.format(parent))
         for filename in file_names:
             if re.match(r'^w', filename):
                 file_path = os.path.join(parent, filename)
-                output = open(CUT_WORDS_RESULT, 'w', encoding='utf-8')
                 with open(file_path, 'r', encoding='utf-8') as content:
                     for text_num, line in enumerate(content):
                         line = line.strip('\n')
@@ -61,9 +50,8 @@ def cut_word():
                             if is_chinese(word):
                                 output.write(word + ' ')
                         output.write('\n')
-                output.close()
                 logger.info('file name：{} finished!'.format(filename))
-                print('file name：{} finished!'.format(filename))
+    output.close()
 
 
 @decorator
@@ -76,18 +64,24 @@ def train_model():
 @decorator
 def predict_model():
     model = Word2Vec.load(MODEL_PATH)
-    s_1, s_2 = 's1', 's2'
+    word_1, word_2 = '春天', '季节'
+    s_1, s_2 = 'Error', 'Error'
     try:
         # 查看两个词相似度
-        s_1 = model.similarity('教师', '老师')
+        s_1 = model.similarity(word_1, word_2)
         # 查看和 这个词最相关的几个词
-        s_2 = model.most_similar('数学')
-    except KeyError:
+        s_2 = model.most_similar(word_1)
+    except KeyError as e:
+        print(str(e))
         pass
-    print(s_1)
-    print(s_2)
+    logger.info('The similarity between {} and {} similarity is {}!'.format(word_1, word_2, str(s_1)))
+    logger.info('与 {} 相似度最高的词有：{} !'.format(word_1, str(s_2)))
+    print('The similarity between {} and {} similarity is {}!'.format(word_1, word_2, str(s_1)))
+    print('与 {} 相似度最高的词有：{} !'.format(word_1, str(s_2)))
     # 查看这个词的 词向量
-    print(model.wv['数学'])
+    vector = model.wv[word_2]
+    logger.info('{} 的词向量为：{} !'.format(word_2, str(vector)))
+    print('{} 的词向量为：{} !'.format(word_2, str(vector)))
 
 
 def tsne_plot():
@@ -118,11 +112,24 @@ def tsne_plot():
     plt.show()
 
 
+def save_log():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    handler = logging.FileHandler('word2vec.log', mode='w')
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+    return logger
+
+
 if __name__ == '__main__':
     logger = save_log()
     DATA_PATH = '../../zhwiki/'
-    CUT_WORDS_RESULT = 'cut_words_result'
-    MODEL_PATH = 'Word2Vec.model'
+    CUT_WORDS_RESULT = '../../cut_words_result'
+    MODEL_PATH = 'word2vec.model'
     cut_word()
     train_model()
     predict_model()
